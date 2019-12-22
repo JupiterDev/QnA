4e7d8c0fe8ffc3196b71ac97f8f6dc19e8f8bd65
JupiterDev <ilyaklyus@gmail.com>
2019-12-22 20:16:18 +0300

    User can add links to the answer

--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -10,7 +10,7 @@ feature 'User can add links to answer', %q{
   given!(:question) {create(:question, user: user)}
   given(:gist_url) {'https://gist.github.com/vkurennov/743f9367caa1039874af5a2244e1b44c'}
 
-  scenario 'User adds link when give an answer', js: true do
+  scenario 'User adds links when give an answer', js: true do
     sign_in(user)
 
     visit question_path(question)
@@ -18,13 +18,22 @@ feature 'User can add links to answer', %q{
     fill_in 'Body', with: 'My answer'
 
     fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+    fill_in 'Link url', with: gist_url
+
+    click_on 'Add link'
+
+    within all('.nested-fields')[1] do
+      fill_in 'Link name', with: 'My another gist'
+      fill_in 'Link url', with: gist_url
+    end
 
     click_on 'Answer'
 
     within '.answers' do
       expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'My another gist', href: gist_url
     end
+
   end
 
 end
